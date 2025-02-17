0b269d02d4233a511de86bd1a4e4f864a21d1e6d
CosmoConnectt <projectuse882@gmail.com>
2025-02-17 23:05:27 +0530

    Fix server port to use Render's dynamic PORT

--- index.js
+++ index.js
@@ -45,7 +45,12 @@ app.use((error, req, res, next) => {
 });
 
 // Start Server
-app.listen(3000, () => {
-  connectDB();
-  console.log("✅ Server is running on port 3000!");
+// app.listen(3000, () => {
+//   connectDB();
+//   console.log("✅ Server is running on port 3000!");
+// });
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
